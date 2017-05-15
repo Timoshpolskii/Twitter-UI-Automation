ea7221d1c6b436c241bb6657c2acb86a6882f70d
Timoshpolskii <andrewtimoshpolskiy@MacBook-Pro-Andrew.local>
2017-05-15 11:05:45 +0300

    Added okhttp-signpost to handle OAuth authentication

--- src/main/java/API/Requests/UserTimeline.java
+++ src/main/java/API/Requests/UserTimeline.java
@@ -4,18 +4,9 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 
 
 public interface UserTimeline {
-    @Headers({
-            "Authorization: OAuth oauth_consumer_key=\"z7tNU6PYcciYUrnsXXXRapDqM\"," +
-                    "oauth_token=\"3390666947-cfbg4q7A13ip5tPqtXBd6bZ48FWsSzpgPgrtzee\"," +
-                    "oauth_signature_method=\"HMAC-SHA1\"," +
-                    "oauth_timestamp=\"1494687890\"," +
-                    "oauth_nonce=\"ThdHFv\"," +
-                    "oauth_version=\"1.0\"," +
-                    "oauth_signature=\"uzVFRkna%2BRBIlFRXuVDxdjnNTrY%3D\""})
     @GET("1.1/statuses/user_timeline.json")
     Call<ResponseBody> getUserPosts(@Header("screen_name") String header);
 }
--- src/main/java/API/Support/SingletonRetrofit.java
+++ src/main/java/API/Support/SingletonRetrofit.java
@@ -1,7 +1,10 @@
 package main.java.API.Support;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class SingletonRetrofit {
 
@@ -12,9 +15,20 @@ public class SingletonRetrofit {
     public static  synchronized Retrofit getRetrofit() {
 
         if (retrofit == null) {
+
+            OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer("z7tNU6PYcciYUrnsXXXRapDqM",
+                    "zS40mN5Xw24uu69A3l0EY5XXQ5PIat03byZqDfJCcc5SxXlLBt");
+            consumer.setTokenWithSecret("3390666947-cfbg4q7A13ip5tPqtXBd6bZ48FWsSzpgPgrtzee",
+                    "qvL3gk3r8OlSjm9J4rCeaBny9GNvSSqJOscz1mcc6fs4u");
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new SigningInterceptor(consumer))
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl("https://api.twitter.com/")
+                    .client(client)
                     .build();
         }
         return retrofit;
